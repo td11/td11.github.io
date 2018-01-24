07dae1a5cc2345d4c9a82130ee023014c5b90952
td11 <33686827+td11@users.noreply.github.com>
2018-01-24 23:35:18 +0000

    Ordenador
    Datosimagenes json incluido intentando poner un div que se cargue cada vez que una peli se seleccione

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,2 +1,24 @@
 //Eliminar banner de publicidad
 $('a[title="CSS jQuery"]').remove();
+
+$(function () {
+    
+    // When the user clicks on <span> (x), close the modal
+    $("div .close").onclick = function () {
+        $("#myModal").style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == $("#myModal")) {
+            $("#myModal").style.display = "none";
+        }
+    }
+});
+
+
+
+function ventanaModal(elemento) {
+    var id = $(elemento).attr("id");
+    $("body #modal" + id + " .modal").css('display', 'block');
+}
